9eccbfcc64d5964a0834c7be93a4007a5362b787
dipanshu <medipansh@gmail.com>
2022-06-20 02:27:24 +0530

    0.0.1

---
+++ 02_basics.js
@@ -0,0 +1,33 @@
+// comments - single line 
+/* comments - multiple line */
+
+// 1.Variables
+// There are multiple ways to create a new variables
+var number3 = 45;   //global scope - mostly avoided
+let number1 = 23;   //block scope - most used
+const number2 = 34; // constant, global scope
+
+// 2. Data Types
+// Js is dynamically typed. so, it automatically decides the type of data.
+
+// 3. Operators - used to perform an operation
+// 3.a - Assignment Operator
+console.log(2+3); // adds
+console.log(2-3); // substracts
+console.log(2*3); // multiply
+console.log(2/3); // devides
+console.log(2%3); // modulus
+console.log(++number1); // increment
+console.log(--number1); // decrement
+
+// 3.b - Comparasion Operator - returns boolean value
+console.log(2 == 3);  // checks the equality
+console.log(2 === 3); // checks the equality and data types
+console.log(2 != 3);  // not equal to
+console.log(2 !== 3); // identical or not
+console.log(2 < 3);   // less than
+console.log(2 <= 3);   // less than or equal to
+console.log(2 > 3);   // greater than 
+console.log(2 >= 3);   // greater than
+
+// 3.c - 
